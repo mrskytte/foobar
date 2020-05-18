a74e3a318af2b6a73bc246127541809e645de7e0
Chris <mr.skytte@me.com>
2020-05-18 16:26:54 +0200

    Re-wrote code to clear out hotfix miscommunication

--- script.js
+++ script.js
@@ -1,60 +1,100 @@
 "use strict";
 
 window.addEventListener("load", init);
+
 let domTickets = [];
 let lastQueue = [];
 let currentQueue = [];
-let counter = 10;
 
 const endpoint = "https://fireoranges.herokuapp.com";
 
 function init() {
   domTickets = document.querySelectorAll(".queue-entry");
   getQueueSpacing();
+  isQueueMoveDone();
   getInitialQueue();
   prepareQueue();
-  isQueueMoveDone();
-  //   setInterval(artificialQueue, 3000);
+}
+
+function getQueueSpacing() {
+  const queuePos1 = document
+    .querySelector("[data-position=one]")
+    .getBoundingClientRect().x;
+  const queuePos2 = document
+    .querySelector("[data-position=two]")
+    .getBoundingClientRect().x;
+  const queueOffset = queuePos1 - queuePos2;
+  document
+    .getElementById("container")
+    .style.setProperty("--queue-offset", queueOffset);
+}
+
+function isQueueMoveDone() {
+  document
+    .querySelector("[data-position=one]")
+    .addEventListener("animationend", removeMoveQueueClass);
+
+  document
+    .querySelector("[data-position=one]")
+    .addEventListener("animationend", moveQueue);
+
+  function removeMoveQueueClass() {
+    domTickets.forEach((entry) => entry.classList.remove("move-queue"));
+  }
 }
 
 async function getInitialQueue() {
   const data = await fetch(endpoint);
   const response = await data.json();
+
   currentQueue = [...response.queue];
-  console.log(currentQueue);
-  fillOutQueueArray();
   prepareQueue();
   startSystem();
 }
 
 function prepareQueue() {
   const queue = [...domTickets];
+  lastQueue = [...currentQueue];
+
+  fillOutQueueArray();
+
   domTickets.forEach((ticket) => ticket.classList.add("hide-queue"));
+
   for (let i = 0; i < currentQueue.length; i++) {
     const id =
       currentQueue[i].id > 100
         ? currentQueue[i].id.toString().substring(1)
         : currentQueue[i].id;
-    queue[i].classList.remove("hide-queue");
-    if (queue[i].querySelector("p").textContent === "hideme") {
-      queue[i].classList.add("show-queue");
-    }
-    setTimeout(() => queue[i].classList.remove("show-queue"), 1000);
-    queue[i].querySelector("p").textContent = id;
-    if (queue[i].querySelector("p").textContent === "hideme") {
+
+    if (id !== "hideme") {
+      queue[i].classList.remove("hide-queue");
+    } else {
       queue[i].classList.add("hide-queue");
     }
+
+    if (
+      queue[i].querySelector("p").textContent === "hideme" &&
+      id !== "hideme"
+    ) {
+      queue[i].classList.add("show-queue");
+      setTimeout(() => queue[i].classList.remove("show-queue"), 1000);
+    }
+
+    queue[i].querySelector("p").textContent = id;
+
+    // if (queue[i].querySelector("p").textContent === "hideme") {
+    //   queue[i].classList.add("hide-queue");
+    // }
   }
-  lastQueue = [...currentQueue];
 }
 
 function fillOutQueueArray() {
-  lastQueue = [...currentQueue];
   if (currentQueue.length < 10) {
     for (let i = currentQueue.length; i < 9; i++) {
       currentQueue.push({ id: "hideme" });
     }
   }
+  currentQueue.length = 9;
 }
 
 function startSystem() {
@@ -75,46 +115,36 @@ function checkQueueProgress(queue) {
     console.log("adding new entries");
     prepareQueue();
   }
-  if (!currentQueue[0]) {
-    console.log("queue empty");
+  if (!currentQueue[0] && !lastQueue[0]) {
+    console.log("empty arrays and nothing new");
+  } else if (!currentQueue[0] && lastQueue[0]) {
+    console.log("Queue emptied out");
+    setIterations(lastQueue.length);
+    lastQueue = [...currentQueue];
+    updateQueue();
   } else if (!lastQueue[0] && currentQueue[0]) {
     console.log("new entry + old queue empty ");
+    lastQueue = [...currentQueue];
+
     updateQueue();
   } else if (lastQueue[0].id !== currentQueue[0].id) {
-    if (lastQueue[1].id === currentQueue[0].id) {
+    if (lastQueue.length < 2) {
+    } else if (lastQueue[1].id === currentQueue[0].id) {
       setIterations(1);
-      updateQueue();
     } else if (lastQueue[2].id === currentQueue[0].id) {
       setIterations(2);
     }
+    lastQueue = [...currentQueue];
+
     console.log("new entry");
     updateQueue();
   } else if (lastQueue[0].id === currentQueue[0].id) {
     console.log("no change");
   } else {
+    lastQueue = [...currentQueue];
+
     updateQueue();
   }
-}
-
-function setIterations(iterations) {
-  document
-    .querySelector("#container")
-    .style.setProperty("--iterations", iterations);
-}
-
-function isQueueMoveDone() {
-  document
-    .querySelector("[data-position=one]")
-    .addEventListener("animationend", removeMoveQueueClass);
-  document
-    .querySelector("[data-position=one]")
-    .addEventListener("animationend", () => {
-      moveQueue();
-    });
-}
-
-function removeMoveQueueClass() {
-  domTickets.forEach((entry) => entry.classList.remove("move-queue"));
 }
 
 function updateQueue() {
@@ -122,23 +152,10 @@ function updateQueue() {
   domTickets.forEach((entry) => entry.classList.add("move-queue"));
 }
 
-function getQueueSpacing() {
-  console.log(
-    "queuePos1",
-    document.querySelector("[data-position=one]").getBoundingClientRect()
-  );
-  const queuePos1 = document
-    .querySelector("[data-position=one]")
-    .getBoundingClientRect().x;
-  const queuePos2 = document
-    .querySelector("[data-position=two]")
-    .getBoundingClientRect().x;
-
-  const queueOffset = queuePos1 - queuePos2;
-  console.log(queueOffset);
+function setIterations(iterations) {
   document
-    .getElementById("container")
-    .style.setProperty("--queue-offset", queueOffset);
+    .querySelector("#container")
+    .style.setProperty("--iterations", iterations);
 }
 
 function moveQueue() {
@@ -168,5 +185,3 @@ function showActiveTickets() {
     })
   );
 }
-
-// currentQueue[i].id > 100 ? currentQueue[i].id - 100 : currentQueue[i].id;
