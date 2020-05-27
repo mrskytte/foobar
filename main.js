154c7acf520c97ed27b16b0b60e871303801556e
mijaworski <michjawo@gmail.com>
2020-05-27 13:40:42 +0200

    Merging the queue part and fixing the on tap today part

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ function start() {
 async function loadJSON() {
     const response = await fetch("beers.json");
     const jsonData = await response.json();
+    console.log(jsonData);
 
     // when loaded, prepare data objects
     prepareObjects(jsonData);
@@ -33,31 +34,43 @@ function prepareObjects(jsonData) {
 }
 
 function prepareObject(jsonObject) {
+    console.log(jsonObject);
     const beer = Object.create(Beer);
+    beer.image = jsonObject.image;
+    beer.name = jsonObject.name;
+    beer.type = jsonObject.type;
+    beer.alc = jsonObject.alc;
+    beer.price = jsonObject.price;
+
 
     return beer;
 }
 
 function displayList(beers) {
+    console.log(beer);
     // clear the list
-    document.querySelector("#list > tbody").innerHTML = "";
+    document.querySelector("#list tbody").innerHTML = "";
     // build a new list
     beers.forEach(displayBeer);
 }
 
 function displayBeer(beer) {
+
+
     // create clone
-    const clone = document.querySelector("#beer").content.cloneNode(true);
+    const clone = document.querySelector("template#beer").content.cloneNode(true);
     // set clone data
     clone.querySelector("[data-field=image]").textContent = beer.image;
     clone.querySelector("[data-field=name]").textContent = beer.name;
     clone.querySelector("[data-field=type]").textContent = beer.type;
+    clone.querySelector("[data-field=type]").textContent = beer.alc;
     clone.querySelector("[data-field=price]").textContent = beer.price;
     // append clone to list
-    document.querySelector("#list > tbody").appendChild(clone);
+    document.querySelector("#list tbody").appendChild(clone);
+
 }
 
 function showBeer(beer) {
-    const template = document.querySelector("template").content;
-    const copy = template.cloneNode(true);
+    const template = document.querySelector("#beer").content;
+    const clone = template.cloneNode(true);
 }
